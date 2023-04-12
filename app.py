241004a74aafcd1a7fa514b9f2c119e975daa89e
dfmoura <diogomou@gmail.com>
2023-04-12 14:55:01 -0300

    atualizar cotacao

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
 import requests
 import pandas as pd
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda='EUR'&@dataInicial='04-01-2023'&@dataFinalCotacao='04-11-2023'&$top=100&$format=json&$select=cotacaoVenda,dataHoraCotacao,tipoBoletim"
-
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        data = response.json()
-        df = pd.json_normalize(data["value"])
-        df = df[["cotacaoVenda", "dataHoraCotacao", "tipoBoletim"]]
-        df = df[df["tipoBoletim"] == "Fechamento"]
-        return render_template("index.html", data=df.to_html())
-    else:
-        return "Failed to retrieve data."
-
-if __name__ == "__main__":
-    app.run()
+@app.route('/exchange_rate', methods=['GET'])
+def exchange_rate():
+	moeda = request.args.get('moeda')
+	dataInicial = request.args.get('dataInicial')
+	dataFinalCotacao = request.args.get('dataFinalCotacao')
+	url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda=" + moeda + "&@dataInicial=" + dataInicial + "&@dataFinalCotacao=" + dataFinalCotacao + "&$top=100&$format=json&$select=cotacaoVenda,dataHoraCotacao,tipoBoletim"
+	response = requests.get(url)
+	if response.status_code == 200:
+		data = response.json()
+		df = pd.json_normalize(data["value"])
